d65ca99f4dec83b8b8679bc1307a9928feb3579e
Madeline Dutro <dutro.madeline@gmail.com>
2020-07-08 12:44:33 -0400

    trying to add onto merged branch - we'll see how this goes

--- controllers/emojis.php
+++ controllers/emojis.php
@@ -4,8 +4,16 @@ include_once __DIR__ . '/../models/emoji.php';
 header('Content-Type: application/json');
 
 if ($_REQUEST['action'] === 'index'){
-  echo 'hi fren';
   echo json_encode(Emojis::all());
+} elseif ($_REQUEST['action'] === 'post'){
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $new_emoji = new Emoji(null, $body_object->name, $body_object->referenceimg, $body_object->description);
+  echo json_encode(Emojis::create($new_emoji));
+} elseif ($_REQUEST['action'] === 'update'){
+
+} elseif ($_REQUEST['action'] === 'delete'){
+
 }
 
 ?>
